8306c4d844e19bc04e854265a452d49b2a7ed923
Joe Klinck <jklinck76@gmail.com>
2025-04-12 04:03:03 -0700

    updated HashMaps.java

--- hashMaps.java
+++ hashMaps.java
@@ -1,31 +1,40 @@
-// HashMaps
-HashMap<Integer,Integer> map1 = new HashMap<>();
-LinkedHashMap<Integer,Integer> map2 = new LinkedHashMap<>();
+class HashMaps{
 
-// creates a key of 5 with a value of 17
-map.put(5,17);
-// creates a key of 6 with a value of 45
-map.put(6,45);
+      public static void main(String[] args) {
+            
+            // creates a hashmap
+            HashMap<Integer,Integer> map = new HashMap<>();
 
-// this will check if the map contains a key with the value of 5, it is NOT 
-// checking the value held in that key
-map.containsKey(5); 
+            // creates and ordered hashmaps since hahsmaps are not 
+            // necessarily ordered
+            LinkedHashMap<Integer,Integer> map2 = new LinkedHashMap<>();
 
-// this will return the value held where 5 is the key
-map.get(5);
+            // creates a key of 5 with a value of 17
+            map.put(5,17);
+            // creates a key of 6 with a value of 45
+            map.put(6,45);
 
-// this will print the entire map in this form {5=17, 6=45}
-System.out.println(map);
+            // this will check if the map contains a key with the value of 5, it is NOT 
+            // checking the value held in that key
+            map.containsKey(5); 
 
-// this loop through the map and prints out the keys
-for(Integer key: map.keySet()) {
-      System.out.println(key);
-}
+            // this will return the value held where 5 is the key
+            map.get(5);
 
-for(Map.Entry<Integer,Integer> entry: map.entrySet()) {
-      System.out.println(entry.getKey()); // prints the key
-      System.out.println(entry.getValue()); // prints the value
-      System.out.printf("%d: %d\n", entry.getKey(), entry.getValue()); // prints both
+            // this will print the entire map in this form {5=17, 6=45}
+            System.out.println(map);
+
+            // this loops through the map and prints out the keys
+            for(Integer key: map.keySet()) {
+                  System.out.println(key);
+            }
+
+            for(Map.Entry<Integer,Integer> entry: map.entrySet()) {
+                  System.out.println(entry.getKey()); // prints the key
+                  System.out.println(entry.getValue()); // prints the value
+                  System.out.printf("%d: %d\n", entry.getKey(), entry.getValue()); // prints both
+            }
+      }
 }
 
 
